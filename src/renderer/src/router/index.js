d95718028ec960137b5a7a7968e3355542d2203d
Kazunori OYA <terje@oya3.net>
2024-01-27 14:48:35 +0900

    add package of chart.js and vue-chartjs

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/chart',
+    name: 'Chart',
+    component: () => import('../views/Chart.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     component: () => import('../views/About.vue')
