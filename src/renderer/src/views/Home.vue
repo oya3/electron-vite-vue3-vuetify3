59c82a53bdc84003707078fdd5246234ddcc5fd9
Kazunori OYA <terje@oya3.net>
2024-01-27 20:08:24 +0900

    add package of vue3-treeview

--- src/renderer/src/views/Home.vue
+++ src/renderer/src/views/Home.vue
@@ -3,5 +3,52 @@
     <v-btn>Button</v-btn>
     <v-chip>Chip</v-chip>
   </div>
+  <div>
+     <Tree :config="config" :nodes="nodes"></Tree>
+  </div>
 </template>
+
+
+<script setup>
+import 'vue3-treeview/dist/style.css';
+import Tree from 'vue3-treeview';
+import { ref, reactive } from 'vue';
+
+const config = ref({
+    roots: ["id1", "id2"], // 第一階層のkey
+    checkboxes: true, // チェックボックスあり
+    checkMode: 0, // 親階層をチェックした際に子階層もチェックする
+    padding: 22,
+});
+
+const nodes = reactive({
+    id1: {
+        text: "text1",
+        children: ["id11", "id12"],
+        state: {
+            opened: true, // デフォルトで開く
+        }
+    },
+    id11: {
+        text: "text1-1",
+        children: [],
+    },
+    id12: {
+        text: "text1-2",
+        children: [],
+    },
+    id2: {
+        text: "text2",
+        children: ["id21"],
+    },
+    id21: {
+        text: "text2-1",
+        children: ["id211"],
+    },
+    id211: {
+        text: "text2-1-1",
+        children: [],
+    },
+});
+</script>
 
