fc482b6a8a6c7baf2f65eb2f5196c2a132d8706e
Kazunori OYA <terje@oya3.net>
2024-02-01 23:38:19 +0900

    add icons using @mdi/js

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,10 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/chart">Chart</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<v-app>
+  <v-app-bar color="primary">
+
+    <v-app-bar-nav-icon 
+      variant="text"
+      @click.stop="drawer = !drawer">
+    </v-app-bar-nav-icon>
+
+    <v-app-bar-title>
+      Application
+    </v-app-bar-title>
+  </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+  >
+    <!-- <v-list nav> -->
+    <!--   <v-list-item prepend-icon="mdiAccount" title="Home" value="home"></v-list-item> -->
+    <!--   <v-list-item prepend-icon="mdiAccount" title="About" value="about"></v-list-item> -->
+    <!-- </v-list> -->
+    <v-list nav>
+      <v-list-item title="Home" value="home">
+        <template v-slot:prepend>
+          <v-icon :icon="mdiAccount" />
+        </template>
+      </v-list-item>
+      <v-list-item title="About" value="about">
+        <template v-slot:prepend>
+          <v-icon :icon="mdiAccount" />
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+  <v-main>
+    <v-container>
+      <v-icon :icon="mdiAccount" />Main Contents
+    </v-container>
+  </v-main>
+</v-app>
 </template>
+
+<script setup>
+import { mdiAccount } from '@mdi/js'
+import { ref } from 'vue'
+let drawer = ref(null)
+</script>
+
+<!-- <script> -->
+<!-- import { mdiAccount } from '@mdi/js' -->
+<!-- export default { -->
+<!--   data: () => ({ -->
+<!--     mdiAccount, -->
+<!--     drawer: null, -->
+<!--   }), -->
+<!-- } -->
+<!-- </script> -->
--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -3,7 +3,16 @@ import App from './App.vue';
 import 'vuetify/styles';  // vuetifyパレットカラーを利用するためにはGlobal CSSの”vuetify/styles”のimportが必要
 // import './assets/main.css'
 import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 import router from "./router"; // src/router/index.js を参照
 
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    }
+  }
+});
 createApp(App).use(vuetify).use(router).mount('#app');
