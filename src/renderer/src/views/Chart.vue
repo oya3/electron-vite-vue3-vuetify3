62f47e40b0fccd91bf5fdfc23d934999d1b58dda
Kazunori OYA <terje@oya3.net>
2024-02-03 16:57:40 +0900

    refactoring. update stacked bar chart

--- src/renderer/src/views/Chart.vue
+++ src/renderer/src/views/Chart.vue
@@ -1,13 +1,29 @@
 <template>
   <Bar
-  id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-    :width="600"
-    :height="400"
+    id="my-chart-id"
+    :options="chartOptions2"
+    :data="chartData2"
   />
-  <!-- <div style="width: 100%; height: 100%;"> -->
-  <!-- </div> -->
+  <div>  <!-- <v-container class="bg-surface-variant"> -->
+    <v-row style="width: 100%; height: 100%;">
+      <v-col
+        cols="6"
+      >
+        <Bar
+          :options="chartOptions"
+          :data="chartData"
+          />
+      </v-col>
+      <v-col
+        cols="6"
+      >
+        <Bar
+          :options="chartOptions"
+          :data="chartData"
+          />
+      </v-col>
+    </v-row>
+  </div>   <!-- </v-container> -->
 </template>
 
 <script>
@@ -22,34 +38,34 @@ export default {
   components: { Bar },
   data() {
     return {
-      chartData: {
-        labels:  ["00年", "05年", "10年", "15年", "20年"],
+      chartData2: {
+        labels:  ["00年", "05年", "10年", "15年", "20年", "00年", "05年", "10年", "15年", "20年", "00年", "05年", "10年", "15年", "20年", "00年", "05年", "10年", "15年", "20年"],
         datasets: [
           {
             label: "系列Ａ",
-            data: [10, 20,  5, 15, 50],
+            data: [10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50],
             backgroundColor: "red"
           },
           {
             label: "系列Ｂ",
-            data: [ 5, 10, 10,  5,  8],
+            data: [ 5, 10, 10,  5,  8, 10, 20,  5, 15, 50,  5, 10, 10,  5,  8, 10, 20,  5, 15, 50],
             backgroundColor: "blue"
           },
           {
             label: "系列C",
-            data: [ 15, 20, 20,  15,  18],
+            data: [ 15, 20, 20,  15,  18, 10, 20,  5, 15, 50,  15, 20, 20,  15,  18, 10, 20,  5, 15, 50],
             backgroundColor: "green"
           }
         ]
       },
-      chartOptions: {
+      chartOptions2: {
         responsive: true,
         scales: {
           x: {
-              stacked: true  // 積み上げの指定
+            stacked: true,  // 積み上げの指定
           },
           y: {
-            stacked: true  //  積み上げの指定
+            stacked: true,  //  積み上げの指定
           }
         },
         plugins: {
@@ -59,10 +75,85 @@ export default {
             text: "積上げ棒グラフ"
           },
           zoom: {
+            pan: {
+              // pan options and/or events
+              enabled: true, // パンを有効にする
+              // mode: 'xy', // 'x'| 'y'|'xy' 'xy' 許可されるパン方向
+              // scaleMode: 'xy',
+            },
+            limits: {
+              // axis limits
+              x: {min: 0, max: 200},
+              y: {min: 0, max: 200},
+              // x: {min: 0},
+              // y: {min: 0},
+            },
             zoom: {
               wheel: {
                 enabled: true,
-                speed: 0.01,  // ズームの速度を調整
+                modifierKey: 'ctrl',
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'xy',
+            }
+          }
+        }
+      },
+      chartData: {
+        labels:  ["00年", "05年", "10年", "15年", "20年", "00年", "05年", "10年", "15年", "20年", "00年", "05年", "10年", "15年", "20年", "00年", "05年", "10年", "15年", "20年"],
+        datasets: [
+          {
+            label: "系列Ａ",
+            data: [10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50],
+            backgroundColor: "red"
+          },
+          {
+            label: "系列Ｂ",
+            data: [ 5, 10, 10,  5,  8, 10, 20,  5, 15, 50,  5, 10, 10,  5,  8, 10, 20,  5, 15, 50],
+            backgroundColor: "blue"
+          },
+          {
+            label: "系列C",
+            data: [ 15, 20, 20,  15,  18, 10, 20,  5, 15, 50,  15, 20, 20,  15,  18, 10, 20,  5, 15, 50],
+            backgroundColor: "green"
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,  // 積み上げの指定
+          },
+          y: {
+            stacked: true,  //  積み上げの指定
+          }
+        },
+        plugins: {
+          title: {
+            display: true,
+            fontSize: 20,
+            text: "積上げ棒グラフ"
+          },
+          zoom: {
+            pan: {
+              // pan options and/or events
+              enabled: true, // パンを有効にする
+              // mode: 'xy', // 'x'| 'y'|'xy' 'xy' 許可されるパン方向
+              // scaleMode: 'xy',
+            },
+            limits: {
+              // axis limits
+              x: {min: 0, max: 200},
+              y: {min: 0, max: 200},
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+                // speed: 0.0001,  // ズームの速度を調整
+                modifierKey: 'ctrl',
               },
               pinch: {
                 enabled: true
