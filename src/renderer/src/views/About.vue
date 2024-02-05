09b0e7b535648c00f21351bff71714bf1464a25f
Kazunori OYA <terje@oya3.net>
2024-02-06 00:22:19 +0900

    refactoring

--- src/renderer/src/views/About.vue
+++ src/renderer/src/views/About.vue
@@ -1,4 +1,7 @@
 <template>
+  <v-icon :icon="mdiFolder" />folder
+  <v-icon :icon="mdiFolderOpen" />open
+
   <div class="about">
     <h1>This is an about page</h1>
     <Versions />
@@ -8,10 +11,17 @@
 <script>
 // import Versions from '../components/Versions.vue'
 import Versions from '@renderer/components/Versions.vue'
+import { mdiFolderOpen, mdiFolder } from '@mdi/js'
 
 export default {
   components: {
     Versions
+  },
+  data() {
+    return {
+      mdiFolder,
+      mdiFolderOpen
+    }
   }
 }
 </script>
