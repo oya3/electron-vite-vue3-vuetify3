071502b8e62eaff31c6ab6ef19df6ade2ede42c5
Kazunori OYA <terje@oya3.net>
2024-02-07 13:11:21 +0900

    add vue3-tree-vue

--- src/renderer/src/views/About.vue
+++ src/renderer/src/views/About.vue
@@ -6,21 +6,76 @@
     <h1>This is an about page</h1>
     <Versions />
   </div>
+
+  <div>
+    <Vue3TreeVue
+      :items="vue3TreeVueItems"
+      :isCheckable="true"
+      :hideGuideLines="false">
+      <template v-slot:item-prepend-icon="treeViewItem" >
+        <v-icon v-if="treeViewItem.children">
+          {{ treeViewItem.expanded ? mdiFolderOpen : mdiFolder }}
+        </v-icon>
+        <!-- <v-icon>{{ mdiFolder }}</v-icon> -->
+      </template>
+    </Vue3TreeVue>
+  </div>
 </template>
 
 <script>
 // import Versions from '../components/Versions.vue'
 import Versions from '@renderer/components/Versions.vue'
 import { mdiFolderOpen, mdiFolder } from '@mdi/js'
+import Vue3TreeVue from 'vue3-tree-vue'
+import 'vue3-tree-vue/dist/style.css';
 
 export default {
   components: {
-    Versions
+    Versions,
+    Vue3TreeVue
   },
   data() {
     return {
       mdiFolder,
-      mdiFolderOpen
+      mdiFolderOpen,
+      vue3TreeVueItems: [
+        {
+          id: 1,
+          name: 'Node 1',
+          children: [
+            {
+              id: 2,
+              name: 'Node 1.1',
+              children: [
+                {
+                  id: 3,
+                  name: 'Node 1.1.1'
+                }
+              ]
+            },
+            {
+              id: 10,
+              name: 'Node 10.1',
+              children: [
+                {
+                  id: 11,
+                  name: 'Node 10.1.1'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 4,
+          name: 'Node 2',
+          children: [
+            {
+              id: 5,
+              name: 'Node 2.1'
+            }
+          ]
+        }
+      ],
     }
   }
 }
