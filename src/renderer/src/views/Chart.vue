f670507403c5f2522662526163887898f0b22585
Kazunori OYA <terje@oya3.net>
2024-02-09 00:21:56 +0900

    add CombinedLine chart

--- src/renderer/src/views/Chart.vue
+++ src/renderer/src/views/Chart.vue
@@ -26,7 +26,7 @@
                 />
             </div>
           </div>
-          <div style="width: 100%; height: 100%;"> 
+          <div style="width: 100%; height: 400px;"> 
             <v-row style="width: 100%; height: 100%;">
               <v-col
                 cols="6"
@@ -49,8 +49,25 @@
               </v-col>
             </v-row>
           </div>
+          <div style="width: 100%; max-height: 650px;"> 
+            <div style="width: 100%; height: 100%;">
+              <v-btn @click="reset_chart">reset</v-btn>
+              <Line
+                ref="chartCombinedBar"
+                :options="chartOptionsCombinedLineBar"
+                :data="chartDataCombinedLineBar"
+                />
+            </div>
+          </div>
         </v-col>
       </v-row>
+      <!-- <div> -->
+      <!--   <Line -->
+      <!--     ref="chartCombinedLine" -->
+      <!--     :options="chartOptionsCombinedLine" -->
+      <!--     :data="chartDataCombinedLine" -->
+      <!--     /> -->
+      <!-- </div> -->
   </div>
 </template>
 
@@ -182,7 +199,7 @@ export default {
               10, 20,  5, 15, 50
             ],
             backgroundColor: "rgba(120,60,243,0.8)",
-          }
+          },
         ]
       },
       chartOptionsBar: {
@@ -314,7 +331,161 @@ export default {
       chartOptionsPie: {
         responsive: true,
         // maintainAspectRatio: false,
-      }
+      },
+      // CombinedLineBar
+      chartDataCombinedLineBar: {
+        labels:  [
+          "2000年", "2001年", "2002年", "2003年", "2004年",
+          "2005年", "2006年", "2007年", "2008年", "2009年",
+          "2010年", "2011年", "2012年", "2013年", "2014年",
+          "2015年", "2016年", "2017年", "2018年", "2019年"
+        ],
+        datasets: [
+          {
+            label: "系列Ａ",
+            data: [
+              10, 20,  5, 15, 50,
+              10, 20,  5, 15, 50,
+              10, 20,  5, 15, 50,
+              10, 20,  5, 15, 50
+            ],
+            // backgroundColor 背景色 rgba(0, 0, 0, 0.1)
+            // borderColor ボーダの色 rgba(0, 0, 0, 0.1)
+            // 文字の色 #666
+            backgroundColor: "rgba(18,63,166,0.8)",
+            stack: 'combined',
+            type: 'bar'
+
+          },
+          {
+            label: "系列Ｂ",
+            data: [
+              5, 10, 10,  5,  8,
+              10, 20,  5, 15, 50,
+              5, 10, 10,  5,  8,
+              10, 20,  5, 15, 50
+            ],
+            backgroundColor: "rgba(26,170,222,0.8)",
+            stack: 'combined',
+            type: 'bar'
+          },
+          {
+            label: "系列C",
+            data: [
+              15, 20, 20, 15, 18,
+              10, 20,  5, 15, 50,
+              15, 20, 20, 15, 18,
+              10, 20,  5, 15, 50
+            ],
+            backgroundColor: "rgba(120,60,243,0.8)",
+            stack: 'combined',
+            type: 'bar'
+          },
+          {
+            label: "期待値",
+            data: [
+              110, 112, 110, 114, 110,
+              112, 110, 114, 110, 30,
+              110, 113, 110, 112, 114,
+              110, 111, 110, 114, 10,
+            ],
+            backgroundColor: "rgba(200,200,200,0.8)",
+            stack: 'combined',
+          }
+        ]
+      },
+      chartOptionsCombinedLineBar: {
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,  // 積み上げの指定
+            grid: {
+              color: 'rgba(128, 128, 128, 0.5)',  // light, dark テーマ両方でグリッド線が見える色に変更
+            },
+          },
+          y: {
+            stacked: true,  //  積み上げの指定
+            grid: {
+              color: 'rgba(128, 128, 128, 0.5)',  // light, dark テーマ両方でグリッド線が見える色に変更
+            },
+          }
+        },
+        plugins: {
+          title: {
+            display: true,
+            fontSize: 20,
+            text: "積上げ棒グラフ"
+          },
+          zoom: {
+            pan: {
+              // pan options and/or events
+              enabled: true, // パンを有効にする
+              // mode: 'xy', // 'x'| 'y'|'xy' 'xy' 許可されるパン方向
+              // scaleMode: 'xy',
+            },
+            limits: {
+              // axis limits
+              x: {min: 0, max: 200},
+              y: {min: 0, max: 200},
+              // x: {min: 0},
+              // y: {min: 0},
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+                modifierKey: 'ctrl',
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'xy',
+            }
+          }
+        }
+      },
+
+      // // combined_Line
+      // chartDataCombinedLine: {
+      //   labels:  ["00時", "01時", "02時", "03時", "04時",
+      //             "05時", "06時", "07時", "08時", "09時",
+      //             "10時", "11時", "12時", "13時", "14時",
+      //             "15時", "16時", "17時", "18時", "19時"],
+      //   datasets: [
+      //     {
+      //       label: "Bar1",
+      //       data: [10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50],
+      //       backgroundColor: "red",
+      //       stack: 'combined',
+      //       type: 'bar'
+      //     },
+      //     {
+      //       label: "Bar1",
+      //       data: [10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50, 10, 20,  5, 15, 50],
+      //       backgroundColor: "green",
+      //       stack: 'combined',
+      //       type: 'bar'
+      //     },
+      //     {
+      //       label: "Line1",
+      //       data: [ 5, 10, 10,  5,  8, 10, 20,  5, 15, 50,  5, 10, 10,  5,  8, 10, 20,  5, 15, 50],
+      //       backgroundColor: "blue",
+      //       stack: 'combined'
+      //     },
+      //   ]
+      // },
+      // chartOptionsCombinedLine: {
+      //   plugins: {
+      //     title: {
+      //       display: true,
+      //       text: 'Chart.js Stacked Line/Bar Chart'
+      //     }
+      //   },
+      //   scales: {
+      //     y: {
+      //       stacked: true
+      //     }
+      //   }
+      // },
     }
   }
 }
