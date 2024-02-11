993810b0374dea289b6ccb47ce5eb1b87494dfe8
Kazunori OYA <terje@oya3.net>
2024-02-11 17:48:45 +0900

    update .eslint rolues

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,27 @@ module.exports = {
     'vue',
   ],
   'rules': {
+    // ['error', 'always'] 中括弧の前後に常にスペースが必要です。
+    // ['error', 'never'] 中括弧の前後にスペースを入れてはいけません。
+    // ['off'], 無効
+    'object-curly-spacing': ['off'],
+    // ['error', 'always'] 文の末尾に常にセミコロンが必要です。
+    // ['error', 'never'] 文の末尾にセミコロンを入れてはいけません。
+    // ['off'], 無効
+    'semi': ['off'],
+    // ['error','always'] オブジェクトや配列の最後の要素の後に常にカンマが必要です。
+    // ['error','never'] オブジェクトや配列の最後の要素の後にカンマを入れてはいけません。
+    // ['off'], 無効
+    'comma-dangle': ['off'],
+    // ['error','always'] すべての関数宣言にJSDocコメントが必要です。
+    // ['error','never'] 関数宣言にJSDocコメントを入れてはいけません。
+    // ['off'] このルールを無効にします。
+    'require-jsdoc': ['error','always'],
+    // ['error','always'] 関数名と括弧の間に常にスペースが必要です。
+    // ['error','never'] 関数名と括弧の間にスペースを入れてはいけません。
+    // ['off'] このルールを無効にします。
+    'space-before-function-paren': ['off'],
+    // 許可行数
+    'max-len': 120,
   },
 };
