f961faf628838ab6117ed8877a2a2f8f92c12c79
Kazunori OYA <terje@oya3.net>
2024-02-24 11:07:32 +0900

    refactoring

--- src/renderer/src/utils/CameraAnimator.js
+++ src/renderer/src/utils/CameraAnimator.js
@@ -3,17 +3,21 @@ import * as THREE from 'three';
 // カメラ移動制御
 class CameraAnimator {
   // カメラ位置
+  static rotatePoints = [
+    { x: 0, y: 4, z: 7 }, // 回転時のオフセット
+    { x: 0, y: 5, z: 0 }, // 回転注視点
+  ]
+  // カメラ位置
   static movePoints = [
-    { x: -10, y: 0, z: 0 }, // point1 屋根
-    { x: 10, y: 10, z: 0 }, // point2 柱
+    { x: -7, y: 5, z: 0 }, // point1 屋根
+    { x: 7, y: 5, z: 0 }, // point2 柱
     { x: 0, y: 7, z: 7 }, // 回転時のオフセット
   ];
-  static midPoint = { x: 0, y: 10, z: 0 }; // 中間用
+  static midPoint = { x: 0, y: 5, z: 0 }; // 注視点中間用
   // 注目点
   static targetPoints = [
     { x: -2.093, y: 8.291, z: 2.110 }, // point1 屋根
     { x: 1.2558, y: 0.950, z: -0.955 }, // point2 柱
-    { x: 0, y: 5, z: 0 }, // 回転
   ];
 
   // CameraAnimatorクラスの新しいインスタンスを作成します。
@@ -30,18 +34,16 @@ class CameraAnimator {
   changeAction(mode, params) {
     if (mode == 1) {
       this.action = new MoveAction(
-        this.camera, CameraAnimator.movePoints[2], CameraAnimator.movePoints[0],
-        CameraAnimator.targetPoints[0], 3000);
+        this.camera, CameraAnimator.movePoints[2], CameraAnimator.movePoints[0], CameraAnimator.targetPoints[0], 3000);
       return;
     } else if (mode == 2) {
       this.action = new MoveAction(
-        this.camera, CameraAnimator.movePoints[2], CameraAnimator.movePoints[1],
-        CameraAnimator.targetPoints[1], 3000);
+        this.camera, CameraAnimator.movePoints[2], CameraAnimator.movePoints[1], CameraAnimator.targetPoints[1], 3000);
       return;
     }
     // else{
     // }
-    this.action = new RotateAction(this.camera, CameraAnimator.movePoints[2], CameraAnimator.targetPoints[2]);
+    this.action = new RotateAction(this.camera, CameraAnimator.rotatePoints[0], CameraAnimator.rotatePoints[1]);
   }
 
   // アニメーション実行
@@ -81,19 +83,34 @@ class RotateAction {
 
 // ２点移動アクション
 class MoveAction {
-  // @param {Object} cameraPosition - カメラ位置
-  // @param {Object} targetPosition - 注視点位置
+  // @param {Object} camera - カメラ
   // @param {Object} midPosition - 中間位置
-  // @param {Object} startPosition - 終了位置
+  // @param {Object} endPosition - 終了位置
+  // @param {Object} endLookAt - 注視点位置
   // @param {number} duration - アニメーションの期間（ミリ秒）
-  constructor(camera, midPosition, endPosition, cameraLookAtTarget, duration) {
+  constructor(camera, midPosition, endPosition, endLookAt, duration) {
     this.camera = camera;
     this.startPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
     this.midPosition = new THREE.Vector3(midPosition.x, midPosition.y, midPosition.z);
     this.endPosition = new THREE.Vector3(endPosition.x, endPosition.y, endPosition.z);
-    this.cameraLookAtPosition = new THREE.Vector3(cameraLookAtTarget.x, cameraLookAtTarget.y, cameraLookAtTarget.z);
+    this.curve = new THREE.CatmullRomCurve3([
+      this.startPosition,
+      this.midPosition,
+      this.endPosition,
+    ]);
 
-
+    // カメラの向きを取得（これはオイラー角です）
+    const cameraDirection = this.camera.getWorldDirection(new THREE.Vector3());
+    // 視線の終点を計算（カメラの位置 + カメラの向き）
+    this.startLookAt = new THREE.Vector3().addVectors(this.camera.position, cameraDirection);
+    // this.midLookAt = new THREE.Vector3(midPosition.x, midPosition.y, midPosition.z);
+    this.midLookAt = new THREE.Vector3(CameraAnimator.midPoint.x, CameraAnimator.midPoint.y, CameraAnimator.midPoint.y);
+    this.endLookAt = new THREE.Vector3(endLookAt.x, endLookAt.y, endLookAt.z);
+    this.curveLookAt = new THREE.CatmullRomCurve3([
+      this.startLookAt,
+      this.midLookAt,
+      this.endLookAt,
+    ]);
     this.duration = duration;
     this.start = null;
     this.progress = 0;
@@ -103,19 +120,14 @@ class MoveAction {
     if (this.progress == 1) return this.progress;
     if (!this.start) this.start = currentTime;
     const elapsed = currentTime - this.start;
-
     this.progress = Math.min(elapsed / this.duration, 1);
     this.progress = this.progress * (2 - this.progress); // easeInOutQuad
-
-    let position;
-    if (this.progress < 0.5) {
-      position = new THREE.Vector3().lerpVectors(this.startPosition, this.midPosition, this.progress * 2);
-    } else {
-      position = new THREE.Vector3().lerpVectors(this.midPosition, this.endPosition, (this.progress - 0.5) * 2);
-    }
+    // スプライン曲線上の現在の位置を取得
+    const position = this.curve.getPoint(this.progress);
+    // const lookAtPosition = new THREE.Vector3().lerpVectors(this.startLookAt, this.endLookAt, this.progress);
+    const lookAtPosition = this.curveLookAt.getPoint(this.progress);
     this.camera.position.copy(position);
-    // const currentPosition = new THREE.Vector3().lerpVectors(startPosition, endPosition, progress);
-    this.camera.lookAt(this.cameraLookAtPosition);
+    this.camera.lookAt(lookAtPosition);
     return this.progress;
   }
 }
