85c2bcd82a322d757a8b663ce909ae7638d97c56
Kazunori OYA <terje@oya3.net>
2024-02-24 15:22:50 +0900

    refactoring

--- src/renderer/src/utils/CameraAnimator.js
+++ src/renderer/src/utils/CameraAnimator.js
@@ -4,20 +4,24 @@ import * as THREE from 'three';
 class CameraAnimator {
   // カメラ位置
   static rotatePoints = [
-    { x: 0, y: 4, z: 7 }, // 回転時のオフセット
+    { x: 0, y: 7, z: 7 }, // カメラ位置(回転時のオフセット)
     { x: 0, y: 5, z: 0 }, // 回転注視点
   ]
-  // カメラ位置
-  static movePoints = [
-    { x: -7, y: 5, z: 0 }, // point1 屋根
-    { x: 7, y: 5, z: 0 }, // point2 柱
-    { x: 0, y: 7, z: 7 }, // 回転時のオフセット
+
+  static movePoints1 = [
+    { x: -6, y: 5, z: 0 }, // カメラ位置 point1 屋根
+    { x: -2.093, y: 8.291, z: 2.110 }, // 注視点 point1 屋根
+    { x: 0, y: 5, z: 0 }, // 注視点中間用
+    // { x: 1.2558, y: 0.950, z: -0.955 }, // 注視点 point2 柱
+    // { x: -2.093, y: 8.291, z: 2.110 }, // 注視点 point1 屋根
   ];
-  static midPoint = { x: 0, y: 5, z: 0 }; // 注視点中間用
-  // 注目点
-  static targetPoints = [
-    { x: -2.093, y: 8.291, z: 2.110 }, // point1 屋根
-    { x: 1.2558, y: 0.950, z: -0.955 }, // point2 柱
+
+  static movePoints2 = [
+    { x: 6, y: 5, z: 0 }, // カメラ位置 point2 柱
+    { x: 1.2558, y: 0.950 + 2, z: -0.955 }, // 注視点 point2 柱
+    { x: 0, y: 5, z: 0 }, // 注視点中間用
+    // { x: -2.093, y: 8.291, z: 2.110 }, // 注視点 point1 屋根
+    // { x: 1.2558, y: 0.950, z: -0.955 }, // 注視点 point2 柱
   ];
 
   // CameraAnimatorクラスの新しいインスタンスを作成します。
@@ -33,17 +37,13 @@ class CameraAnimator {
   // @param {Object} params - パラメータ
   changeAction(mode, params) {
     if (mode == 1) {
-      this.action = new MoveAction(
-        this.camera, CameraAnimator.movePoints[2], CameraAnimator.movePoints[0], CameraAnimator.targetPoints[0], 3000);
+      this.action = new MoveAction(this.camera, CameraAnimator.movePoints1, 3000);
       return;
     } else if (mode == 2) {
-      this.action = new MoveAction(
-        this.camera, CameraAnimator.movePoints[2], CameraAnimator.movePoints[1], CameraAnimator.targetPoints[1], 3000);
+      this.action = new MoveAction(this.camera, CameraAnimator.movePoints2, 3000);
       return;
     }
-    // else{
-    // }
-    this.action = new RotateAction(this.camera, CameraAnimator.rotatePoints[0], CameraAnimator.rotatePoints[1]);
+    this.action = new RotateAction(this.camera, CameraAnimator.rotatePoints);
   }
 
   // アニメーション実行
@@ -54,18 +54,15 @@ class CameraAnimator {
   }
 }
 
-
 // 回転アクション
 class RotateAction {
   // @param {Object} camera - カメラオブジェクト
-  // @param {Object} offset - 注視点からのオフセット
-  // @param {Object} target - lookup position
-  constructor(camera, offset, target) {
+  // @param {Object} points - 入力座標パラメータ
+  constructor(camera, points) {
     // offset
     this.camera = camera;
-    this.offset = new THREE.Vector3( offset.x, offset.y, offset.z);
-    this.cameraLookAtTarget = new THREE.Vector3(target.x, target.y, target.z);
-    // this.camera.lookAt(this.cameraLookAtTarget); // 注視点位置
+    this.offset = new THREE.Vector3( points[0].x, points[0].y, points[0].z);
+    this.cameraLookAtTarget = new THREE.Vector3(points[1].x, points[1].y, points[1].z);
   }
 
   // @param {Number} offset_y - lookup potion からのオフセット
@@ -84,18 +81,19 @@ class RotateAction {
 // ２点移動アクション
 class MoveAction {
   // @param {Object} camera - カメラ
-  // @param {Object} midPosition - 中間位置
-  // @param {Object} endPosition - 終了位置
-  // @param {Object} endLookAt - 注視点位置
+  // @param {Object} points - 入力座標パラメータ
   // @param {number} duration - アニメーションの期間（ミリ秒）
-  constructor(camera, midPosition, endPosition, endLookAt, duration) {
+  constructor(camera, points, duration) {
+    // points[0] カメラ位置 point1 屋根
+    // points[1] 注視点 point1 屋根
+    // points[2] 注視点中間用
     this.camera = camera;
     this.startPosition = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
-    this.midPosition = new THREE.Vector3(midPosition.x, midPosition.y, midPosition.z);
-    this.endPosition = new THREE.Vector3(endPosition.x, endPosition.y, endPosition.z);
+    this.midPosition = new THREE.Vector3(points[2].x, points[2].y, points[2].z);
+    this.endPosition = new THREE.Vector3(points[0].x, points[0].y, points[0].z);
     this.curve = new THREE.CatmullRomCurve3([
       this.startPosition,
-      this.midPosition,
+      // this.midPosition,
       this.endPosition,
     ]);
 
@@ -104,11 +102,11 @@ class MoveAction {
     // 視線の終点を計算（カメラの位置 + カメラの向き）
     this.startLookAt = new THREE.Vector3().addVectors(this.camera.position, cameraDirection);
     // this.midLookAt = new THREE.Vector3(midPosition.x, midPosition.y, midPosition.z);
-    this.midLookAt = new THREE.Vector3(CameraAnimator.midPoint.x, CameraAnimator.midPoint.y, CameraAnimator.midPoint.y);
-    this.endLookAt = new THREE.Vector3(endLookAt.x, endLookAt.y, endLookAt.z);
+    this.midLookAt = new THREE.Vector3(points[2].x, points[2].y, points[2].z);
+    this.endLookAt = new THREE.Vector3(points[1].x, points[1].y, points[1].z);
     this.curveLookAt = new THREE.CatmullRomCurve3([
       this.startLookAt,
-      this.midLookAt,
+      // this.midLookAt,
       this.endLookAt,
     ]);
     this.duration = duration;
@@ -124,8 +122,8 @@ class MoveAction {
     this.progress = this.progress * (2 - this.progress); // easeInOutQuad
     // スプライン曲線上の現在の位置を取得
     const position = this.curve.getPoint(this.progress);
-    // const lookAtPosition = new THREE.Vector3().lerpVectors(this.startLookAt, this.endLookAt, this.progress);
-    const lookAtPosition = this.curveLookAt.getPoint(this.progress);
+    const lookAtPosition = new THREE.Vector3().lerpVectors(this.startLookAt, this.endLookAt, this.progress);
+    // const lookAtPosition = this.curveLookAt.getPoint(this.progress);
     this.camera.position.copy(position);
     this.camera.lookAt(lookAtPosition);
     return this.progress;
