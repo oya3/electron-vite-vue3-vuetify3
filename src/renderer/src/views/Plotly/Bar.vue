4b924b053a63cd33c286d01c0534132528d00e2a
Kazunori OYA <terje@oya3.net>
2024-07-12 23:37:58 +0900

    refactoring

--- src/renderer/src/views/Plotly/Bar.vue
+++ src/renderer/src/views/Plotly/Bar.vue
@@ -1,34 +1,88 @@
 <template>
-  <div ref="graph"></div>
+  <div>{{clickBarName}}</div>
+  <div id="graph"></div>
 </template>
 
 <script setup>
-import { onMounted, reactive, ref, watch } from "vue";
+import { onMounted, inject, reactive, ref, watch } from "vue";
 import Plotly from "plotly.js-dist-min";
+const darkTheme = inject('darkTheme');
+const vuetify = inject('vuetify');
+const clickBarName = ref('');
 
-const graph = ref(null); // ref<HTMLDivElement>();
-const data = reactive([
-  {
-    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+// データの準備
+const data = reactive([{
+  x: ['ネズミ', 'リス', '犬', '猫', 'サル', 'マングース'],
+  y: [20, 14, 23, 20, 14, 23],
+  type: 'bar',
+  marker: {
+    color: ['rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(255, 255, 0)', 'rgb(0, 255, 255)', 'rgb(255, 0, 255)'], // 棒ごとの色を変更
   },
-]);
+}]);
+
+// レイアウトの設定
+const layout = reactive({
+  title: '動物の数',
+  font: {
+    family: 'Raleway, sans-serif',
+  },
+  showlegend: false,
+  xaxis: {
+    tickangle: -45,
+  },
+  yaxis: {
+    zeroline: false,
+    gridwidth: 2,
+  },
+  bargap: 0.05,
+  plot_bgcolor: 'rgb(233, 233, 233)', // 背景色を変更
+  paper_bgcolor: 'rgb(100, 233, 233)', // 背景色を変更
+});
+
+// コンフィグの設定
+const config = {
+  responsive: true, // レスポンシブにする
+  displaylogo: false, // Plotlyのロゴを削除
+  modeBarButtonsToRemove: [ // 削除するツールボタン
+    // 'zoom2d',
+    // 'pan2d',
+    // 'select2d',
+    // 'lasso2d',
+    // 'zoomIn2d',
+    // 'zoomOut2d',
+    // 'autoScale2d',
+    // 'resetScale2d',
+    // 'hoverClosestCartesian',
+    // 'hoverCompareCartesian',
+    // 'toggleSpikelines',
+    // 'toImage',
+  ],
+
+};
 
 onMounted(() => {
-  if (!graph.value) return;
-  Plotly.newPlot(graph.value, data);
+  Plotly.newPlot( 'graph', data, layout, config);
+  const graphDiv = document.getElementById('graph');
+  graphDiv.on('plotly_click', function(data) {
+    const xValue = data.points[0].x;
+    clickBarName.value = xValue;
+    console.log(xValue + 'がクリックされました');
+  });
 });
 
-setInterval(() => {
-  data[0].y[3] += 1;
-  for (let i = 0; i < data[0].y.length; i++) {
-    data[0].y[i] = Math.random();
+watch(darkTheme, (newValue) => {
+  if (true) { // renderer が生成されてないと背景色を設定できない
+    if (darkTheme.value) {
+      layout.plot_bgcolor = 'rgb(123, 123, 123)'
+      layout.paper_bgcolor = 'rgb(110, 123, 123)'
+    } else {
+      layout.plot_bgcolor = 'rgb(233, 233, 233)'
+      layout.paper_bgcolor = 'rgb(100, 233, 233)'
+    }
+    Plotly.redraw( 'graph', data, layout, config);
+    // Plotly.redraw(graph.value);
   }
-}, 2000);
-
-watch(data, () => {
-  if (!graph.value) return;
-  Plotly.redraw(graph.value);
-});
+  console.log('darkTheme.value:', newValue);
+}, { immediate: false }); // trueの場合、初期化時にも呼ばれる。falseの場合、変更後に呼ばれる
 
 </script>
